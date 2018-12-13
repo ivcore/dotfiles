7765665ea1fab4fcc6689b970e43f8cafcbf955b
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2018-12-13 16:56:11 -0400

    Add two time related functions/aliases to fish
    Like to work timestamping things, but I don't wanna remember the
    arguments to use in each case for the `time` command, so I made two
    functions that will serve as aliases or a quick reference in case I
    wanna remember said arguments. I leave pending adding the functions to
    bash (and zsh I guess)

---
+++ config/.config/fish/functions/shortdate.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function shortdate --description 'alias shortdate=date +%Y%m%d%H%M%S'
+	date +%Y%m%d%H%M%S $argv;
+end
---
+++ config/.config/fish/functions/unixdate.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 0
+function unixdate --description 'alias unixdate=date +%s'
+	date +%s $argv;
+end
