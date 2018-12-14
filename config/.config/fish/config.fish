6594bbb6f2d99d574fc6c96d75ee0b9ca6eeeda9
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2018-12-14 13:38:05 -0400

    Improve PATHing for bash and fish

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -7,11 +7,10 @@ set -gx fish_prompt_pwd_dir_length 0
 # If Ruby exists, we want to have it's bins available.
 if command -v ruby > /dev/null
   set -l ruby_dir (ruby -e 'print Gem.user_dir')
-  set -l ruby_bin_dir $ruby_dir/bin
-
+  set -l ruby_bin_dir "$ruby_dir/bin"
   if test ! -d $ruby_bin_dir
+    echo "Creating $ruby_bin_dir"
     mkdir -p $ruby_bin_dir
-    echo "Created $ruby_bin_dir directory"
   end
 
   set -x GEM_HOME $ruby_dir
@@ -20,10 +19,10 @@ end
 
 # How can we miss Rust?
 if command -v cargo > /dev/null
-  set -l cargo_bin_dir ~/.cargo/bin
+  set -l cargo_bin_dir "$HOME/.cargo/bin"
   if test ! -d $cargo_bin_dir
+    echo "Creating $cargo_bin_dir"
     mkdir -p $cargo_bin_dir
-    echo "Created $cargo_bin_dir directory"
   end
 
   set -x PATH $PATH $cargo_bin_dir
@@ -32,10 +31,9 @@ end
 # The same applies to Yarn
 if command -v yarn > /dev/null
   set -l yarn_bin_dir (yarn global bin 2> /dev/null)
-
   if test ! -d $yarn_bin_dir
+    echo "Creating $yarn_bin_dir"
     mkdir -p $yarn_bin_dir
-    echo "Created $yarn_bin_dir directory"
   end
 
   set -x PATH $PATH $yarn_bin_dir
