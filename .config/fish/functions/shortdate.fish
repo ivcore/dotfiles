f2d5e05b684e4bc8a44c8e62550a64b2cb26eb8f
Ivan Gonzalez Polanco <ivan14polanco@gmail.com>
2020-11-02 02:51:46 -0400

    Update scripts

--- .config/fish/functions/shortdate.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function shortdate --description 'alias shortdate=date +%Y%m%d%H%M%S'
-	date +%Y%m%d%H%M%S $argv;
-end
--- .config/fish/functions/unixdate.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function unixdate --description 'alias unixdate=date +%s'
-	date +%s $argv;
-end
